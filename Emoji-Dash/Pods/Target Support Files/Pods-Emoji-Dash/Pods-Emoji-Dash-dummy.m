e14cc208f2e65ec3ba03c35b29d2b6264d390b0a
Esti Tweg <estictweg@hotmail.com>
2019-01-24 01:47:04 -0500

    Initial Xcode project with gamePlayKit, spriteKit and pop (via pods) imported

---
+++ Emoji-Dash/Pods/Target Support Files/Pods-Emoji-Dash/Pods-Emoji-Dash-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Emoji_Dash : NSObject
+@end
+@implementation PodsDummy_Pods_Emoji_Dash
+@end
---
+++ Emoji-Dash/Pods/Target Support Files/Pods-Emoji-DashTests/Pods-Emoji-DashTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Emoji_DashTests : NSObject
+@end
+@implementation PodsDummy_Pods_Emoji_DashTests
+@end
