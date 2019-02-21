7ab40d9090738c2afb34eda97a35460438420cce
Esti Tweg <estictweg@hotmail.com>
2019-02-20 23:04:06 -0500

    End game scene centred text, more dynamic and flip transition

--- Emoji-Dash/Emoji-Dash/EndGameScene.swift
+++ Emoji-Dash/Emoji-Dash/EndGameScene.swift
@@ -14,23 +14,23 @@ class EndGameScene: SKScene{
     
     init(size: CGSize, won:Bool) {
         super.init(size: size)
-        
+        let displaySize: CGRect = UIScreen.main.bounds
         backgroundColor = SKColor(red: 37/255, green: 67/255, blue: 71/255, alpha: 1.0)
         
         // Score
         let lblScore = SKLabelNode(fontNamed: "ChalkboardSE-Bold")
         lblScore.fontSize = 60
         lblScore.fontColor = SKColor.white
-        lblScore.position = CGPoint(x: 160, y: 300)
+        lblScore.position = CGPoint(x: displaySize.width/2, y: displaySize.height-110)
         lblScore.horizontalAlignmentMode = .center
-        lblScore.text = "\(GameState.sharedInstance.score)"
+        lblScore.text = "Score: \(GameState.sharedInstance.score)"
         addChild(lblScore)
         
         // High Score
         let lblHighScore = SKLabelNode(fontNamed: "ChalkboardSE-Bold")
         lblHighScore.fontSize = 30
         lblHighScore.fontColor = SKColor.white
-        lblHighScore.position = CGPoint(x: 160, y: 150)
+        lblHighScore.position = CGPoint(x: displaySize.width/2, y: lblScore.frame.minY-lblScore.frame.height-5)
         lblHighScore.horizontalAlignmentMode = .center
         lblHighScore.text = "High Score: \(GameState.sharedInstance.highScore)"
         addChild(lblHighScore)
@@ -39,7 +39,7 @@ class EndGameScene: SKScene{
         let lblTryAgain = SKLabelNode(fontNamed: "ChalkboardSE-Bold")
         lblTryAgain.fontSize = 30
         lblTryAgain.fontColor = SKColor.white
-        lblTryAgain.position = CGPoint(x: 160, y: 50)
+        lblTryAgain.position = CGPoint(x: displaySize.width/2, y: lblHighScore.frame.minY-lblHighScore.frame.height-5)
         lblTryAgain.horizontalAlignmentMode = .center
         lblTryAgain.text = "Tap To Try Again"
         addChild(lblTryAgain)
@@ -55,7 +55,7 @@ class EndGameScene: SKScene{
         let gameScene = Game(size: size)
         gameScene.scaleMode = .resizeFill
         gameScene.anchorPoint = CGPoint(x: 0.0, y: 0.0)
-        let reveal = SKTransition.fade(withDuration: 0.5)
+        let reveal = SKTransition.flipHorizontal(withDuration: 0.6)
         view?.presentScene(gameScene, transition: reveal)
         
     }
